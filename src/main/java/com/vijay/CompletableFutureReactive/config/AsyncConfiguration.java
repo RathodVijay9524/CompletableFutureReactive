f470c690d4ec612ca2d756720de0309260ccd3bb
vijay <omvijay44@gmail.com>
2024-10-29 16:14:22 +0530

    LonginAspect added

--- src/main/java/com/vijay/CompletableFutureReactive/config/AsyncConfiguration.java
+++ src/main/java/com/vijay/CompletableFutureReactive/config/AsyncConfiguration.java
@@ -2,6 +2,7 @@ package com.vijay.CompletableFutureReactive.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -17,7 +18,7 @@ public class AsyncConfiguration {
         taskExecutor.setCorePoolSize(4);
         taskExecutor.setQueueCapacity(150);
         taskExecutor.setMaxPoolSize(4);
-        taskExecutor.setThreadNamePrefix("AsyncTaskThread-");
+        taskExecutor.setThreadNamePrefix("AsyncThread-");
         taskExecutor.initialize();;
         return taskExecutor;
     }
--- src/main/java/com/vijay/CompletableFutureReactive/service/PatientService.java
+++ src/main/java/com/vijay/CompletableFutureReactive/service/PatientService.java
@@ -6,6 +6,7 @@ import com.vijay.CompletableFutureReactive.repository.PatientRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,18 +19,19 @@ public class PatientService {
     private final PatientRepository repository;
     private final ApplicationEventPublisher publisher;
 
+    @Async("asyncTaskExecutor")
     public CompletableFuture<List<Patient>> getAllPatients() {
         log.info("Fetching all patients");
         return CompletableFuture.supplyAsync(repository::findAll);
     }
-
+    @Async("asyncTaskExecutor")
     public CompletableFuture<Patient> getPatient(Long id) {
         log.info("Fetching patient with ID: {}", id);
         return CompletableFuture.supplyAsync(() -> {
             return repository.findById(id).orElse(null);
         });
     }
-
+    @Async("asyncTaskExecutor")
     public CompletableFuture<Patient> create(Patient patient) {
         log.info("Saving patient: {}", patient.getName());
         return CompletableFuture.supplyAsync(() -> {
@@ -38,12 +40,12 @@ public class PatientService {
             return savedPatient;
         });
     }
-
+    @Async("asyncTaskExecutor")
     public CompletableFuture<Void> deletePatient(Long id) {
         log.info("Deleting patient with ID: {}", id);
         return CompletableFuture.runAsync(() -> repository.deleteById(id));
     }
-
+    @Async("asyncTaskExecutor")
     public CompletableFuture<Patient> updatePatient(Long id, Patient patientDetails) {
         log.info("Updating patient with ID: {}", id);
         return CompletableFuture.supplyAsync(() -> {

7573a7e9d61f43158395cb72c305191b701bc333
vijay <omvijay44@gmail.com>
2024-11-03 10:57:52 +0530

    Revert "@Acync added"
    This reverts commit 90d1ca6f13adba1465157e91940e4f904a9082d1.

--- src/main/java/com/vijay/CompletableFutureReactive/CompletableFutureCrudApplicationReactiveApplication.java
+++ src/main/java/com/vijay/CompletableFutureReactive/CompletableFutureCrudApplicationReactiveApplication.java
@@ -2,8 +2,7 @@ package com.vijay.CompletableFutureReactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-//@EnableAsync     if we use custom pull of thread the this is optional
+
 @SpringBootApplication
 public class CompletableFutureCrudApplicationReactiveApplication {
 
